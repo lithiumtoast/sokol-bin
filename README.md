0b2eb54521d650175730852bebaf6f8df2a8a3c4
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-05-24 12:47:07 -0400

    Scaffold

--- README.md
+++
@@ -1,1 +0,0 @@
-sokol_app-bin
---
+++ src/sokol_app.m
@@ -0,0 +1,22 @@
+#define SOKOL_IMPL
+#define SOKOL_NO_ENTRY
+
+#include <TargetConditionals.h>
+#if TARGET_OS_MAC
+    #include <Cocoa/Cocoa.h>
+#else
+    #error "Not supported Apple platform."    
+#endif
+
+#if SOKOL_METAL
+    #import <Metal/Metal.h>
+#elif SOKOL_GLCORE33
+    #ifndef GL_SILENCE_DEPRECATION
+        #define GL_SILENCE_DEPRECATION
+    #endif
+    #if TARGET_OS_MAC
+        #include <OpenGL/gl3.h>   
+    #endif
+#endif
+
+#include "sokol_app.h"
